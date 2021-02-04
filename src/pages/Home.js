129f322c19d5a8e383deff71aaec6ff30b93bcd7
HelenB84 <helen.boomsluiter@outlook.com>
2021-02-04 13:55:08 +0100

    Added Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import './Home.css'
 import Header from "../components/Header";
+import masker from '../assets/masker.jpg'
+import massage from '../assets/massage.jpg'
+import harsen from '../assets/harsen.jpg'
+import nails from '../assets/nails.jpg'
+import ilsesalon from '../assets/ilsesalon.jpg'
 
 export default function Home(){
     return(
         <>
             <Header/>
-        <h1 className='home'>BEHANDELINGEN</h1>
+            <div className='behandelingen-container'>
+                <h1 className='behandelingen-home'>BEHANDELINGEN</h1>
+                    <h3 className='behandelingen-home-text1'>MAAK EEN AFSPRAAK EN IK REGEL DE REST</h3>
+                <div className='pics-home'>
+                        <img className='masker' src={masker} alt="gezichtsmasker"/>
+                        <img className='massage' src={massage} alt="massage"/>
+                        <img className='harsen' src={harsen} alt="harsen"/>
+                        <img className='nails' src={nails} alt="nagels"/>
+                </div>
+                <div className='texts-home'>
+                    <h3 className='face-text'>GEZICHTSBEHANDELINGEN</h3>
+                    <h3 className='massage-text'>MASSAGE</h3>
+                    <h3 className='waxing-text'>HARSEN</h3>
+                    <h3 className='nails-text'>MANICURE</h3>
+                </div>
+            </div>
+            <div className='container-over-mij'>
+                <img className='ilse-salon' src={ilsesalon} alt="ilse_salon"/>
+                <div className='home-text-over_mij'>
+                    <h3 className='over-mij-title'>OVER MIJ</h3>
+                    <br/>
+                    <p className='over-mij-p'>Persoonlijke aandacht, rust, kwaliteit, sfeer en hygiene staan bij mij
+                    hoog in het vaandel. <br/><br/>
+                    Op deze website kunt u in uw eigen tempo kennis maken met de
+                    verschillende  behandelingen, die ik in mijn salon uitvoer. <br/><br/>
+                    Als er vragen zijn, kunt u natuurlijk  altijd contact met mij opnemen.</p>
+
+                </div>
+            </div>
+
+
+
             </>
     );
 };
